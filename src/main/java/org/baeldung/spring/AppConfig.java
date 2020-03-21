e6fbb8ce6f04ef0e5d3170dac209b7459bc61df7
Wang Wenlin <sopl.wang@gmail.com>
2020-03-22 07:05:53 +0800

    Minor refactor

--- src/main/java/org/baeldung/spring/AppConfig.java
+++ src/main/java/org/baeldung/spring/AppConfig.java
@@ -3,10 +3,11 @@ package org.baeldung.spring;
 import org.baeldung.security.ActiveUserStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration
+@EnableWebSecurity
 public class AppConfig {
     // beans
 
